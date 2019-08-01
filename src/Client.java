c154f40255b6d8dbcda1c61dfc3ecb9be8a2fc80
CYT823 <tim860823@gmail.com>
2019-08-01 15:18:49 +0800

    edit client
    from lab

--- src/Client.java
+++ src/Client.java
@@ -1,16 +1,21 @@
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import javax.swing.JFrame;
 
 public class Client {
 
 	public static void main(String[] args) {
-		Socket client = new Socket();
-		try {
-			client.connect(new InetSocketAddress("140.123.224.108", 30000));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		JFrame frame = new MainFrame();
+		frame.setSize(400, 600);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+
+//		Socket client = new Socket();
+//		try {
+//			client.connect(new InetSocketAddress("140.123.224.108", 30000));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 }
