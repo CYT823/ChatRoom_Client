3f672b195dfd361da1bd5d113190dd569e70799c
CYT823 <tim860823@gmail.com>
2019-08-02 13:47:23 +0800

    Test writers' differences

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
 import javax.swing.JFrame;
 
 public class Client {
@@ -16,6 +25,32 @@ public class Client {
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
+		
+		Socket client = new Socket();
+		try {
+			client.connect(new InetSocketAddress("127.0.0.1", 30000));
+
+//			成功
+//			OutputStream writer = client.getOutputStream();
+//			writer.write("test".getBytes(), 0, 4);
+//			writer.flush();
+			
+//			失敗
+			PrintWriter writer = new PrintWriter(client.getOutputStream());
+//			writer.write("testWriter");
+			writer.println("test2");
+			writer.flush();
+			
+//			DataOutputStream writer = new DataOutputStream(client.getOutputStream());
+//			writer.writeUTF("testData");
+//			writer.flush();
+			
+//			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+//			writer.write("TESTBuffered");
+//			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
