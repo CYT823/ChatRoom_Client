b5b19b6a5ee74e51f37857dc2eb51f015caa68c3
kcy312 <410677008@mail.nknu.edu.tw>
2019-08-04 22:54:19 +0800

    modify the component location

--- src/Panel/CommunicationPanel.java
+++ src/Panel/CommunicationPanel.java
@@ -9,45 +9,45 @@ import javax.swing.JScrollPane;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CommunicationPanel extends JPanel{
-	JTextField typingBar ;
+public class CommunicationPanel extends JPanel {
+	JTextField typingBar;
 	JButton enterBtn;
-	JButton micBtn; 
+	JButton micBtn;
 	JButton picBtn;
 	JButton phoneBtn;
 	JTextArea textArea;
-	
-	public CommunicationPanel(){
+
+	public CommunicationPanel() {
 		setLayout(null);
-		
+
 		typingBar = new JTextField();
 		typingBar.setBounds(10, 480, 300, 30);
 		add(typingBar);
-		
-		enterBtn =new JButton("發送");
-		enterBtn.setBounds(310,480,70,30);
-		add(enterBtn); 
-		
+
+		enterBtn = new JButton("發送");
+		enterBtn.setBounds(310, 480, 70, 30);
+		add(enterBtn);
+
 		micBtn = new JButton("錄音");
-		micBtn.setBounds(10,520,80,25);
+		micBtn.setBounds(10, 520, 70, 30);
 		add(micBtn);
-		
+
 		picBtn = new JButton("照片");
-		picBtn.setBounds(110,520,80,25);
+		picBtn.setBounds(110, 520, 70, 30);
 		add(picBtn);
-		
+
 		phoneBtn = new JButton("通話");
-		phoneBtn.setBounds(205,520,80,25);
+		phoneBtn.setBounds(205, 520, 70, 30);
 		add(phoneBtn);
-		
-		textArea = new JTextArea(100,10);
+
+		textArea = new JTextArea(100, 10);
 		JScrollPane scrollPane = new JScrollPane(textArea);
-		scrollPane.setBounds(50,50,300,400);
+		scrollPane.setBounds(25, 50, 350, 400);
 		add(scrollPane);
-		
+
 		initial();
 	}
-	
+
 	private void initial() {
 		enterBtn.addActionListener(new ActionListener() {
 
@@ -55,7 +55,7 @@ public class CommunicationPanel extends JPanel{
 			public void actionPerformed(ActionEvent e) {
 				textArea.append(typingBar.getText());
 			}
-			
+
 		});
 	}
 }
