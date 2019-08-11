265f5a5b3db71b6bf1b803c0de79664f666384cd
CYT823 <tim860823@gmail.com>
2019-08-11 11:36:20 +0800

    Add an exception of Socket

--- src/Panel/Receiver.java
+++ src/Panel/Receiver.java
@@ -4,23 +4,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.SocketException;
 
-public class Receiver extends Thread{
+public class Receiver extends Thread {
 	Socket client;
 	CommunicationPanel communicationPanel;
 	BufferedReader reader;
-	
+
 	public Receiver(Socket client, CommunicationPanel communicationPanel) {
 		this.client = client;
 		this.communicationPanel = communicationPanel;
-		
+
 		try {
 			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void run() {
 		String message;
@@ -28,6 +29,8 @@ public class Receiver extends Thread{
 			while ((message = reader.readLine()) != null) {
 				communicationPanel.textArea.append(message + "\n");
 			}
+		} catch (SocketException se) {
+			communicationPanel.textArea.append("伺服器已關閉\n");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
